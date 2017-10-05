76489b2c7069dd1f9ef711626a92396652e7ce97
making <makingx@gmail.com>
2017-10-06 01:11:19 +0900

    Add blob

---
+++ add-blobs.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+DIR=`pwd`
+
+mkdir -p .downloads
+
+pushd .downloads
+
+
+
+if [ ! -f ${DIR}/blobs/berkeleydb/db-5.3.28.tar.gz ];then
+    curl -L -O -J http://download.oracle.com/berkeley-db/db-5.3.28.tar.gz
+    bosh add-blob --dir=${DIR} db-5.3.28.tar.gz berkeleydb/db-5.3.28.tar.gz
+fi
+
+if [ ! -f ${DIR}/blobs/openldap/openldap-2.4.44.tgz ];then
+    curl -L -O -J ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-2.4.44.tgz
+    bosh add-blob --dir=${DIR} openldap-2.4.44.tgz openldap/openldap-2.4.44.tgz
+fi
+
+popd
