8e92ae4f5e529c6d0f02b4d68e974d754f01c647
Toshiaki Maki <tmaki@pivotal.io>
2018-12-03 10:58:39 +0900

    Use s3 blob

--- README.md
+++ README.md
@@ -1,76 +1,12 @@
-# BOSH Release for openldap
-
-## Usage
-
-To use this bosh release, first upload it to your bosh:
 
 ```
-bosh target BOSH_HOST
-git clone https://github.com/cloudfoundry-community/openldap-boshrelease.git
-cd openldap-boshrelease
-bosh upload release releases/openldap-1.yml
+bosh create-release --name=openldap --force --timestamp-version --tarball=/tmp/openldap-boshrelease.tgz &&\
+bosh upload-release /tmp/openldap-boshrelease.tgz &&\
+bosh -d openldap deploy manifest.yml \
+  -v internal_ip=10.244.0.100 \
+  -v ldap_olc_suffix=dc=spring,dc=org \
+  -v ldap_olc_root_dn=cn=admin,dc=spring,dc=org \
+  -v ldap_config_ldif=./config.ldif \
+  -v ldap_user_ldif=./users.ldif \
+  --no-redact
 ```
-
-For [bosh-lite](https://github.com/cloudfoundry/bosh-lite), you can quickly create a deployment manifest & deploy a cluster:
-
-```
-templates/make_manifest warden
-bosh -n deploy
-```
-
-For AWS EC2, create a single VM:
-
-```
-templates/make_manifest aws-ec2
-bosh -n deploy
-```
-
-### Override security groups
-
-For AWS & Openstack, the default deployment assumes there is a `default` security group. If you wish to use a different security group(s) then you can pass in additional configuration when running `make_manifest` above.
-
-Create a file `my-networking.yml`:
-
-``` yaml
----
-networks:
-  - name: openldap1
-    type: dynamic
-    cloud_properties:
-      security_groups:
-        - openldap
-```
-
-Where `- openldap` means you wish to use an existing security group called `openldap`.
-
-You now suffix this file path to the `make_manifest` command:
-
-```
-templates/make_manifest openstack-nova my-networking.yml
-bosh -n deploy
-```
-
-### Development
-
-As a developer of this release, create new releases and upload them:
-
-```
-bosh create release --force && bosh -n upload release
-```
-
-### Final releases
-
-To share final releases:
-
-```
-bosh create release --final
-```
-
-By default the version number will be bumped to the next major number. You can specify alternate versions:
-
-
-```
-bosh create release --final --version 2.1
-```
-
-After the first release you need to contact [Dmitriy Kalinin](mailto://dkalinin@pivotal.io) to request your project is added to https://bosh.io/releases (as mentioned in README above).
--- add-blobs.sh
+++ add-blobs.sh
@@ -25,24 +25,24 @@ DEBS="$DEBS http://kr.archive.ubuntu.com/ubuntu/pool/main/a/apr-util/libaprutil1
 DEBS="$DEBS http://kr.archive.ubuntu.com/ubuntu/pool/main/a/apr-util/libaprutil1-dbd-sqlite3_1.5.4-1build1_amd64.deb"
 DEBS="$DEBS http://kr.archive.ubuntu.com/ubuntu/pool/main/a/apr-util/libaprutil1-ldap_1.5.4-1build1_amd64.deb"
 DEBS="$DEBS http://kr.archive.ubuntu.com/ubuntu/pool/main/l/lua5.1/liblua5.1-0_5.1.5-8ubuntu1_amd64.deb"
-DEBS="$DEBS http://security.ubuntu.com/ubuntu/pool/main/a/apache2/apache2-bin_2.4.18-2ubuntu3.8_amd64.deb"
-DEBS="$DEBS http://security.ubuntu.com/ubuntu/pool/main/a/apache2/apache2-utils_2.4.18-2ubuntu3.8_amd64.deb"
-DEBS="$DEBS http://security.ubuntu.com/ubuntu/pool/main/a/apache2/apache2-data_2.4.18-2ubuntu3.8_all.deb"
-DEBS="$DEBS http://security.ubuntu.com/ubuntu/pool/main/a/apache2/apache2_2.4.18-2ubuntu3.8_amd64.deb"
+DEBS="$DEBS http://kr.archive.ubuntu.com/ubuntu/pool/main/a/apache2/apache2-bin_2.4.18-2ubuntu3.8_amd64.deb"
+DEBS="$DEBS http://kr.archive.ubuntu.com/ubuntu/pool/main/a/apache2/apache2-utils_2.4.18-2ubuntu3.8_amd64.deb"
+DEBS="$DEBS http://kr.archive.ubuntu.com/ubuntu/pool/main/a/apache2/apache2-data_2.4.18-2ubuntu3.8_all.deb"
+DEBS="$DEBS http://kr.archive.ubuntu.com/ubuntu/pool/main/a/apache2/apache2_2.4.18-2ubuntu3.8_amd64.deb"
 DEBS="$DEBS http://kr.archive.ubuntu.com/ubuntu/pool/main/p/php-defaults/php-common_35ubuntu6_all.deb"
-DEBS="$DEBS http://security.ubuntu.com/ubuntu/pool/main/p/php7.0/php7.0-common_7.0.30-0ubuntu0.16.04.1_amd64.deb"
-DEBS="$DEBS http://security.ubuntu.com/ubuntu/pool/main/p/php7.0/php7.0-json_7.0.30-0ubuntu0.16.04.1_amd64.deb"
-DEBS="$DEBS http://security.ubuntu.com/ubuntu/pool/main/p/php7.0/php7.0-opcache_7.0.30-0ubuntu0.16.04.1_amd64.deb"
-DEBS="$DEBS http://security.ubuntu.com/ubuntu/pool/main/p/php7.0/php7.0-readline_7.0.30-0ubuntu0.16.04.1_amd64.deb"
-DEBS="$DEBS http://security.ubuntu.com/ubuntu/pool/main/p/php7.0/php7.0-cli_7.0.30-0ubuntu0.16.04.1_amd64.deb"
-DEBS="$DEBS http://security.ubuntu.com/ubuntu/pool/main/p/php7.0/libapache2-mod-php7.0_7.0.30-0ubuntu0.16.04.1_amd64.deb"
+DEBS="$DEBS http://kr.archive.ubuntu.com/ubuntu/pool/main/p/php7.0/php7.0-common_7.0.32-0ubuntu0.16.04.1_amd64.deb"
+DEBS="$DEBS http://kr.archive.ubuntu.com/ubuntu/pool/main/p/php7.0/php7.0-json_7.0.32-0ubuntu0.16.04.1_amd64.deb"
+DEBS="$DEBS http://kr.archive.ubuntu.com/ubuntu/pool/main/p/php7.0/php7.0-opcache_7.0.32-0ubuntu0.16.04.1_amd64.deb"
+DEBS="$DEBS http://kr.archive.ubuntu.com/ubuntu/pool/main/p/php7.0/php7.0-readline_7.0.32-0ubuntu0.16.04.1_amd64.deb"
+DEBS="$DEBS http://kr.archive.ubuntu.com/ubuntu/pool/main/p/php7.0/php7.0-cli_7.0.32-0ubuntu0.16.04.1_amd64.deb"
+DEBS="$DEBS http://kr.archive.ubuntu.com/ubuntu/pool/main/p/php7.0/libapache2-mod-php7.0_7.0.32-0ubuntu0.16.04.1_amd64.deb"
 DEBS="$DEBS http://kr.archive.ubuntu.com/ubuntu/pool/main/p/php-defaults/libapache2-mod-php_7.0+35ubuntu6_all.deb"
-DEBS="$DEBS http://security.ubuntu.com/ubuntu/pool/main/p/php7.0/php7.0-ldap_7.0.30-0ubuntu0.16.04.1_amd64.deb"
+DEBS="$DEBS http://kr.archive.ubuntu.com/ubuntu/pool/main/p/php7.0/php7.0-ldap_7.0.32-0ubuntu0.16.04.1_amd64.deb"
 DEBS="$DEBS http://kr.archive.ubuntu.com/ubuntu/pool/main/p/php-defaults/php-ldap_7.0+35ubuntu6_all.deb"
-DEBS="$DEBS http://security.ubuntu.com/ubuntu/pool/main/p/php7.0/php7.0-xml_7.0.30-0ubuntu0.16.04.1_amd64.deb"
+DEBS="$DEBS http://kr.archive.ubuntu.com/ubuntu/pool/main/p/php7.0/php7.0-xml_7.0.32-0ubuntu0.16.04.1_amd64.deb"
 DEBS="$DEBS http://kr.archive.ubuntu.com/ubuntu/pool/universe/p/php-defaults/php-xml_7.0+35ubuntu6_all.deb"
 DEBS="$DEBS http://kr.archive.ubuntu.com/ubuntu/pool/main/s/ssl-cert/ssl-cert_1.0.37_all.deb"
-DEBS="$DEBS http://security.ubuntu.com/ubuntu/pool/universe/p/phpldapadmin/phpldapadmin_1.2.2-5.2ubuntu2.1_all.deb"
+DEBS="$DEBS http://kr.archive.ubuntu.com/ubuntu/pool/universe/p/phpldapadmin/phpldapadmin_1.2.2-5.2ubuntu2.1_all.deb"
 
 for deb in $DEBS;do
   f=`echo $deb | python -c 'print raw_input().split("/")[-1]'`
@@ -50,6 +50,8 @@ for deb in $DEBS;do
     curl -L -O -J $deb
     bosh add-blob --dir=${DIR} $f phpldapadmin/$f
   fi
+  # echo $deb
+  # curl -s -I $deb | grep Content-Type
 done
 
 cd -
